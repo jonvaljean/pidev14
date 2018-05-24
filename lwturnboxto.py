c05fa2225973bca300493d96e81e0d1038c7907f
jonvaljean <jonvaljean@gmail.com>
2018-05-24 11:10:37 -0400

    turn box on

--- lwturnboxto.py
+++ lwturnboxto.py
@@ -323,7 +323,7 @@ class lw14:
 
 	
 	#modify this model according to requirements of setting
-NR_ARGS = 1
+NR_ARGS = 2
 
 
 #create dictionary mapping box names to dali addresses
@@ -352,7 +352,7 @@ if __name__ == "__main__":
 	#len = 3, because filename is [0], dali-address is [1], dali-data is[2]
 	if len(sys.argv) == NR_ARGS+1:
 		parm_1 = sys.argv[1]
-		#parm_2 = int(sys.argv[2])
+		parm_2 = int(sys.argv[2])
 		#parm_3 = int(sys.argv[3])
 
 		#print out the args
@@ -368,12 +368,10 @@ if __name__ == "__main__":
 	dali_bus = I2C_values[net_dict[parm_1[0]]]
 	DaliBus_Bar1.SetI2cBus(dali_bus)									#Set I2C-Address to the class
 	dali_device = box_dict[parm_1]
-	dali_value = DALI_MAX
+	dali_value = parm_2
 
 	#Send Data test
-	DaliBus_Bar1.SetDaliAddress(dali_device, LW14_ADR_SINGLE, LW14_MODE_CMD)	#Must be in CMD mode !
-
-	#DaliBus_Bar1.SetDaliAddress(dali_device, LW14_ADR_SINGLE, LW14_MODE_DACP)	    #Set the dali address for send data, in this case single device and DACP bit
+	DaliBus_Bar1.SetDaliAddress(dali_device, LW14_ADR_SINGLE, LW14_MODE_DACP)	    #Set the dali address for send data, in this case single device and DACP bit
 	#DaliBus_Bar1.SetDaliAddress(LW14_BROADCAST, LW14_ADR_GROUP, LW14_MODE_DACP)	#Set the dali as broadcast
 	DaliBus_Bar1.SendData(dali_value)												#Send data into the dali bus
 	DaliBus_Bar1.WaitForReady() 													#Wait until DALI is ready. DON'T FORGET IT!!!!!
