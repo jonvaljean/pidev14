08edcdf629654f0be52c6e4e606eba2517cf0cf1
jonvaljean <jonvaljean@gmail.com>
2018-05-30 09:33:01 -0400

    cycle test at bar

--- lwcycler_test.py
+++ lwcycler_test.py
@@ -48,6 +48,7 @@ if __name__ == "__main__":
 				cmd_element2 = cmd.split(',')[1]
 				print("cmd_element1, cmd_element2 are:  ", cmd_element1, cmd_element2)																
 				dali_bus = I2C_values[net_dict[cmd_element1]]
+				print("dali_bus is  ",dali_bus)
 				DaliBus_Bar1.SetI2cBus(dali_bus)
 				dali_device = grp_dict[cmd_element1]
 				if cmd_element2 == "on": dali_value = onval
