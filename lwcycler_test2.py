f4f8fa5f20f0dab0171a92d79664e3d42cff4201
jonvaljean <jonvaljean@gmail.com>
2018-06-02 08:01:37 -0400

    further work on cycle, using fade test logic

--- lwcycler_test2.py
+++ lwcycler_test2.py
@@ -34,55 +34,55 @@ if __name__ == "__main__":
 		print("Wrong number of parameters")
 		sys.exit(0)
 	
-	print("net_dict is ",net_dict)	
-	print("I2C_values is ",I2C_values)
-	print("grp_dict is  ",grp_dict)
+	#print("net_dict is ",net_dict)	
+	#print("I2C_values is ",I2C_values)
+	#print("grp_dict is  ",grp_dict)
 	
 	F=open(filename)
 	F.seek(0)
-	print("in F.seek")
+	#print("in F.seek")
 	cmd_list = F.read().splitlines()
-	print("cmd_list is  ", cmd_list)
+	#print("cmd_list is  ", cmd_list)
 	while True:
 		for line in cmd_list:
 			print("line is ", line)
 			#sleep(sleeptime)  instead of this, loop until fade bit is 0
 			
 			for cmd in line.split(';'):
-				print("cmd is  ", cmd)
+				#print("cmd is  ", cmd)
 				cmd_element1 = cmd.split(',')[0]
 				cmd_element2 = cmd.split(',')[1]
-				print("cmd_element1, cmd_element2 are:  ", cmd_element1, cmd_element2)																
+				#print("cmd_element1, cmd_element2 are:  ", cmd_element1, cmd_element2)																
 				#print("net_dict value is  ",net_dict[cmd_element1])
 				dali_bus = I2C_values[net_dict[cmd_element1]]
-				print("dali_bus is  ",dali_bus)
+				#print("dali_bus is  ",dali_bus)
 				DaliBus_Bar1.SetI2cBus(dali_bus)
 				dali_device = box_dict[cmd_element1+"1"] # test status of box 1 of the group
-				print("dali_device before QueryStatus - should be 1st box of group is ",dali_device)
+				#print("dali_device before QueryStatus - should be 1st box of group is ",dali_device)
 				DaliBus_Bar1.SetDaliAddress(dali_device, LW14_ADR_SINGLE, LW14_MODE_CMD)	    #Set the dali address for command, try query from cmd
 
 				while(1):
-					sleep(0.5) #lets slow it down a liitle bit was .1 maybe this will work
+					sleep(0.1) #lets slow it down a liitle bit 
 					res = DaliBus_Bar1.QueryStatus()
 					#debug output
-					print("Status res is ", res)
+					#print("Status res is ", res)
 					#if fade-in-progress bit is 0 than finished
 					#if res = -1 error on bus, but "ignore and go on"
 					if (res & 0x10) == 0x00:
-						print("fading ready")
+						#print("fading ready")
 						break;
 						
 				dali_device = grp_dict[cmd_element1]
-				print("dali_device after while - should be group id is ", dali_device)
+				#print("dali_device after while - should be group id is ", dali_device)
 				if cmd_element2 == "on": dali_value = onval
 				if cmd_element2 == "off": dali_value = offval
-				print("dali_bus after while is  ",dali_bus)
-				print("dali_value after while is  ",dali_value)
+				#print("dali_bus after while is  ",dali_bus)
+				#print("dali_value after while is  ",dali_value)
 				DaliBus_Bar1.SetI2cBus(dali_bus)
 				DaliBus_Bar1.SetDaliAddress(dali_device, LW14_ADR_GROUP, LW14_MODE_DACP)	    #Set the dali address for send data, in this case single device and DACP bit
 				DaliBus_Bar1.SendData(dali_value)												#Send data into the dali bus
 				DaliBus_Bar1.WaitForReady() 													#Wait until DALI is ready. DON'T FORGET IT!!!!!
-				sleep(0.200)
+				sleep(0.100)
 
 
 
