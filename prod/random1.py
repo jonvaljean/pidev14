ab58783711a7f3c9c08422d86618ecf93800ccc9
Jon Cooper <jonvaljean@gmail.com>
2018-07-14 17:54:49 +0200

    random lighter first try

--- prod/random1.py
+++ prod/random1.py
@@ -50,55 +50,52 @@ if __name__ == "__main__":
     # print("cmd_list is  ", cmd_list)
     while True:
         sleep(sleeptime)  # instead of this, loop until fade bit is 0
+        box_id_key = random.randint(0,len(box_dict))
+        cmd_element1 = boxes[box_id_key][0]
+        cmd_element2 = 'on'
+        # print("cmd_element1, cmd_element2 are:  ", cmd_element1, cmd_element2)
+        # print("net_dict value is  ",net_dict[cmd_element1])
+        dali_bus = I2C_values[net_dict[cmd_element1]]
+        print("dali_bus is  ", dali_bus)
+        DaliBus_Bar1.SetI2cBus(dali_bus)
+        dali_device = box_dict[cmd_element1 + "1"]  # test status of box 1 of the group
+        # print("dali_device before QueryStatus - should be 1st box of group is ",dali_device)
+        DaliBus_Bar1.SetDaliAddress(dali_device, defs.LW14_ADR_SINGLE,defs.LW14_MODE_CMD)  # Set the dali address for command, try query from cmd
 
-                box_id_key = random.randint(0,len(box_dict))
-                cmd_element1 = boxes[box_id_key][0]
-                cmd_element2 = 'on'
-                # print("cmd_element1, cmd_element2 are:  ", cmd_element1, cmd_element2)
-                # print("net_dict value is  ",net_dict[cmd_element1])
-                dali_bus = I2C_values[net_dict[cmd_element1]]
-                print("dali_bus is  ", dali_bus)
-                DaliBus_Bar1.SetI2cBus(dali_bus)
-                dali_device = box_dict[cmd_element1 + "1"]  # test status of box 1 of the group
-                # print("dali_device before QueryStatus - should be 1st box of group is ",dali_device)
-                DaliBus_Bar1.SetDaliAddress(dali_device, defs.LW14_ADR_SINGLE,
-                                            defs.LW14_MODE_CMD)  # Set the dali address for command, try query from cmd
+        while (1):
+            sleep(0.3)  # lets slow it down a liitle bit
+            res = DaliBus_Bar1.QueryStatus()
+            # debug output
+            print("Status res is ", res)
+            # if fade-in-progress bit is 0 than finished
+            # if res = -1 error on bus, but "ignore and go on"
+            if res == -2:
+                 print("res is -2, no data")
+                 break
+                 if (res & 0x10) == 0x00:
+                     print("fading ready")
+                     break
 
-                while (1):
-                    sleep(0.3)  # lets slow it down a liitle bit
-                    res = DaliBus_Bar1.QueryStatus()
-                    # debug output
-                    print("Status res is ", res)
-                    # if fade-in-progress bit is 0 than finished
-                    # if res = -1 error on bus, but "ignore and go on"
-                    if res == -2:
-                        print("res is -2, no data")
-                        break
-                    if (res & 0x10) == 0x00:
-                        print("fading ready")
-                        break
+            dali_device = box_dict[cmd_element1]
+            print("dali_device after while - should be group id is ", dali_device)
+            if cmd_element2 == "on": dali_value = onval
+            if cmd_element2 == "off": dali_value = offval
+            print("dali_bus after while is  ", dali_bus)
+            print("dali_value after while is  ", dali_value)
+            DaliBus_Bar1.SetI2cBus(dali_bus)
+            DaliBus_Bar1.SetDaliAddress(dali_device, defs.LW14_ADR_SINGLE,                 defs.LW14_MODE_DACP)  # Set the dali address for send data, in this case single device and DACP bit
+            DaliBus_Bar1.SendData(dali_value)  # Send data into the dali bus
+            DaliBus_Bar1.WaitForReady()  # Wait until DALI is ready. DON'T FORGET IT!!!!!
+            sleep(sleeptime)
+            cmd_element2 = 'off'
 
-                dali_device = box_dict[cmd_element1]
-                print("dali_device after while - should be group id is ", dali_device)
-                if cmd_element2 == "on": dali_value = onval
-                if cmd_element2 == "off": dali_value = offval
-                print("dali_bus after while is  ", dali_bus)
-                print("dali_value after while is  ", dali_value)
-                DaliBus_Bar1.SetI2cBus(dali_bus)
-                DaliBus_Bar1.SetDaliAddress(dali_device, defs.LW14_ADR_SINGLE,
-                                            defs.LW14_MODE_DACP)  # Set the dali address for send data, in this case single device and DACP bit
-                DaliBus_Bar1.SendData(dali_value)  # Send data into the dali bus
-                DaliBus_Bar1.WaitForReady()  # Wait until DALI is ready. DON'T FORGET IT!!!!!
-                sleep(sleeptime)
-                cmd_element2 = 'off'
-
-                if cmd_element2 == "on": dali_value = onval
-                if cmd_element2 == "off": dali_value = offval
-                print("dali_bus after while is  ", dali_bus)
-                print("dali_value after while is  ", dali_value)
-                DaliBus_Bar1.SetI2cBus(dali_bus)
-                DaliBus_Bar1.SetDaliAddress(dali_device, defs.LW14_ADR_SINGLE,
+            if cmd_element2 == "on": dali_value = onval
+            if cmd_element2 == "off": dali_value = offval
+            print("dali_bus after while is  ", dali_bus)
+            print("dali_value after while is  ", dali_value)
+            DaliBus_Bar1.SetI2cBus(dali_bus)
+            DaliBus_Bar1.SetDaliAddress(dali_device, defs.LW14_ADR_SINGLE,
                     defs.LW14_MODE_DACP)  # Set the dali address for send data, in this case single device and DACP bit
-                DaliBus_Bar1.SendData(dali_value)  # Send data into the dali bus
-                DaliBus_Bar1.WaitForReady()  # Wait until DALI is ready. DON'T FORGET IT!!!!!
-                sleep(sleeptime)
+            DaliBus_Bar1.SendData(dali_value)  # Send data into the dali bus
+            DaliBus_Bar1.WaitForReady()  # Wait until DALI is ready. DON'T FORGET IT!!!!!
+            sleep(sleeptime)
